43fa5d7e5b9a2a4c2d250f3fb23563177770bb59
leo <e455285@gmail.com>
2017-10-26 17:17:05 +0800

    '001'

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(window).ready(function(){
+
+	// var left, width, cont = "#q-cont";e
+
+	$('step').click(function(){
+		$(this).css("background","#5a6");
+		$('.re').css("background","#d90");
+		next();
+	});
+
+	$('.re').click(function(){
+		$(".op").css("background","#d90");
+		$('.re').css("background","#5a6");
+		$(cont).css("left", "0px");
+	});
+
+	function next() {
+		width = $(cont).width()/4;
+		left = $(cont).css("left").slice(0,-2)*1;
+		left -= width;
+		$(cont).css("left", left+"px");
+	}
+});
